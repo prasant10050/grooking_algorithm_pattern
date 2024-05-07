20e011235a90957ecb7c67dec2e7e174968855a7
Prasant <prasant10050@gmail.com>
2024-05-07 22:57:59 +0530

    update single linked list

--- src/linked_list/single/single_linked_list.py
+++ src/linked_list/single/single_linked_list.py
@@ -7,16 +7,21 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
+        self.size = 0
 
     def append(self, data):
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
+            self.size += 1
             return
+
         last_node = self.head
         while last_node.next is not None:
             last_node = last_node.next
         last_node.next = new_node
+        self.size += 1
 
     def print_list(self):
         current_node = self.head
@@ -28,6 +33,7 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
+        self.size += 1
 
     def insert_after_node(self, prev_node, data):
         if not prev_node:
@@ -36,24 +42,103 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = prev_node.next
         prev_node.next = new_node
+        self.size += 1
 
     def delete_node(self, key):
         current_node = self.head
         if current_node and current_node.data == key:
             self.head = current_node.next
+            self.size -= 1
             current_node = None
             return
 
-        prev = None
+        prev_node = None
         while current_node and current_node.data != key:
-            prev = current_node
+            prev_node = current_node
             current_node = current_node.next
 
         if current_node is None:
             return
 
-        prev.next = current_node.next
+        prev_node.next = current_node.next
+        self.size -= 1
         current_node = None
+
+    def append_v2(self, data):
+        new_node = Node(data)
+        if self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+        self.size += 1
+
+    def append_at_location(self, data, index):
+        current_node = self.head
+        prev_node = self.head
+        new_node = Node(data)
+        count = 1
+        while current_node is not None:
+            if index == 1:
+                new_node.next = current_node
+                self.head = new_node
+                self.size += 1
+                return
+            elif count == index:
+                new_node.next = current_node
+                prev_node.next = new_node
+                self.size += 1
+                return
+            count += 1
+            prev_node = current_node
+            current_node = current_node.next
+
+        if count < index:
+            print("The list has less number of elements")
+
+    def iter(self):
+        current_node = self.head
+
+        while current_node:
+            val = current_node.data
+            current_node = current_node.next
+            yield val
+
+    def search(self, data):
+        for node in self.iter():
+            if node == data:
+                return True
+            return False
+
+    def size_of_list(self):
+        count = 0
+        for node in self.iter():
+            count += 1
+        return count
+
+    def delete_node_at_pos(self, pos):
+        if self.head:
+            current_node = self.head
+            if pos == 0:
+                self.head = current_node.next
+                self.size -= 1
+                current_node = None
+                return
+
+            prev_node = None
+            count = 0
+            while current_node and count != pos:
+                prev_node = current_node
+                current_node = current_node.next
+                count += 1
+
+            if current_node is None:
+                return
+
+            prev_node.next = current_node.next
+            self.size -= 1
+            current_node = None
 
 
 llist = LinkedList()
@@ -67,4 +152,19 @@ llist.insert_after_node(llist.head.next, "E")
 llist.delete_node("B")
 llist.delete_node("E")
 
+llist.append_at_location("B", 2)
+llist.append_at_location("B", 3)
+llist.append_at_location("A", 3)
+
+llist.delete_node_at_pos(0)
+print("---------------------------")
 llist.print_list()
+print("---------------------------")
+for letter in llist.iter():
+    print(letter)
+print("--------Search-------------")
+print(llist.search('A'))
+print(llist.search('F'))
+print("--------Size---------------")
+print(str(llist.size_of_list()))
+print(str(llist.size))
